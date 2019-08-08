4db6ec1b10211da20238edb3d1b50df5a1147016
Roberto Ruiz <rroberto.rruiz@gmail.com>
2019-08-08 00:19:57 -0500

    mid-ex-2

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,45 @@
-interface GameObject {
+interface RenderedObject {
   render(): void;
 }
 
-class MotivationalPoster implements GameObject {
-  private readonly context: CanvasRenderingContext2D;
+class GameObject {
+  protected readonly context: CanvasRenderingContext2D;
 
-  constructor(context: CanvasRenderingContext2D) {
+  public constructor(context: CanvasRenderingContext2D) {
     this.context = context;
   }
 
+  protected getWidth() {
+    return this.context.canvas.width;
+  }
+
+  protected getHeight() {
+    return this.context.canvas.height;
+  }
+}
+
+class MotivationalPoster extends GameObject {
   render() {
-    const canvasWidth = this.context.canvas.width;
-    const canvasHeight = this.context.canvas.height;
-    this.context.fillStyle = "dimgrey";
-    this.context.strokeStyle = "lightgrey";
-    this.context.lineWidth = 5;
-    this.context.rect(75, 50, canvasWidth - 150, canvasHeight - 100);
-    this.context.fill();
-    this.context.stroke();
+    context.strokeStyle = "rgba(0, 0, 100, 0.3)";
 
-    this.context.font = "34px Arial";
-    this.context.strokeStyle = "#FF2222";
-    this.context.fillStyle = "#FFAAAA";
-    this.context.lineWidth = 0.75;
-    this.context.textAlign = "center";
-    const msg = "2D Drawing";
-    this.context.fillText(msg, canvasWidth / 2, 100);
-    this.context.strokeText(msg, canvasWidth / 2, 100);
+    for (let x = 0; x < canvas.width; x += 10) {
+      context.beginPath();
+      context.moveTo(x, 0);
+      context.lineTo(x, canvas.height);
+      context.lineWidth = x % 50 == 0 ? 1 : 0.5;
+      context.stroke();
+    }
 
-    this.context.strokeStyle = "#FFFFFF";
-    this.context.lineWidth = 2;
-    this.context.beginPath();
-    this.context.arc(200, 140, 20, 0, Math.PI * 2);
-    this.context.moveTo(200, 160);
-    this.context.lineTo(200, 220);
-    this.context.moveTo(180, 300);
-    this.context.lineTo(185, 260);
-    this.context.lineTo(200, 220);
-    this.context.lineTo(215, 260);
-    this.context.lineTo(220, 300);
-    this.context.moveTo(240, 130);
-    this.context.lineTo(225, 170);
-    this.context.lineTo(200, 170);
-    this.context.lineTo(175, 180);
-    this.context.lineTo(170, 220);
-    this.context.stroke();
+    for (let y = 0; y < canvas.height; y += 10) {
+      context.beginPath();
+      context.moveTo(0, y);
+      context.lineTo(canvas.width, y);
+      context.lineWidth = y % 50 == 0 ? 1 : 0.5;
+      context.stroke();
+    }
 
-    const msg2 = "its quite easy";
-    this.context.strokeStyle = "#FF2222";
-    this.context.fillStyle = "#FFAAAA";
-    this.context.font = "24px Arial";
-    this.context.strokeText(msg2, canvas.width / 2, 330);
-    this.context.fillText(msg2, canvas.width / 2, 330);
+    // Applies current stroke style and line width to the entire current path.
+    // context.stroke();
   }
 }
 
