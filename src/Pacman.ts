bf47cefb97375a0fb07bdf78226961dabc232107
Roberto Ruiz <rroberto.rruiz@gmail.com>
2019-08-08 13:49:04 -0500

    biting pacman

---
+++ src/Pacman.ts
@@ -0,0 +1,63 @@
+import GameObject from "./GameObject";
+
+class Pacman extends GameObject {
+  private currentX: number = 200;
+  private currentY: number = 200;
+  private radius: number = 100;
+  private howOpen: number = 0;
+  private isOpeningMouth: boolean = true;
+
+  constructor(context) {
+    super(context);
+  }
+
+  private setHowOpen(howOpen: number) {
+    this.howOpen = howOpen;
+  }
+
+  private setIsOpeningMouth(isOpeningMouth: boolean) {
+    this.isOpeningMouth = isOpeningMouth;
+  }
+
+  update() {
+    const step = 0.1;
+
+    if (this.howOpen >= 1) {
+      this.setIsOpeningMouth(false);
+    }
+
+    if (this.howOpen <= 0) {
+      this.setIsOpeningMouth(true);
+    }
+
+    const newMouthValue = this.isOpeningMouth
+      ? this.howOpen + step
+      : this.howOpen - step;
+    console.log(newMouthValue);
+    this.setHowOpen(newMouthValue);
+  }
+
+  render() {
+    const { context, howOpen, radius, currentX, currentY } = this;
+    const maxAngle = 0.25;
+
+    context.save();
+    context.beginPath();
+    context.arc(
+      currentX,
+      currentX,
+      radius,
+      maxAngle * howOpen * Math.PI,
+      (2 - maxAngle * howOpen) * Math.PI
+    );
+    context.lineTo(currentX, currentY);
+    context.fillStyle = "yellow";
+    context.strokeStyle = "black";
+    context.closePath();
+    context.stroke();
+    context.fill();
+    context.restore();
+  }
+}
+
+export default Pacman;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Grid from "./Grid";
+import Pacman from "./Pacman";
 
 const canv = document.getElementById("game-area") as HTMLCanvasElement;
 const context: CanvasRenderingContext2D = canv.getContext("2d");
@@ -6,29 +7,11 @@ const myDrawing = new Grid(context, {
   strokeColor: "teal",
   labelColor: "#333",
 });
+const pacman = new Pacman(context);
 
-const mySecondDrawing = new Grid(context, {
-  strokeColor: "red",
-  includeLabels: false,
-  smallStep: 5,
-  bigStep: 25,
-  secondaryLineWidth: 0.1,
-  mainLineWidth: 0.5,
-});
-
-context.beginPath();
-context.strokeStyle = "#BB0000";
-context.fillStyle = "#005";
-context.lineWidth = 2;
-context.moveTo(50, 50);
-context.lineTo(150, 250);
-context.lineTo(250, 170);
-context.closePath();
-context.fill();
-context.stroke();
-context.fillText("(50, 50)", 30, 40);
-context.fillText("(150, 250)", 130, 260);
-context.fillText("(250, 170)", 255, 175);
-
-mySecondDrawing.render();
-myDrawing.render();
+setInterval(() => {
+  context.clearRect(0, 0, canv.width, canv.height);
+  myDrawing.render();
+  pacman.update();
+  pacman.render();
+}, 1000 / 40);
